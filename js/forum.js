1e146482a333d73e8ed14a253146e19f7346c23e
Prakhar Shukla <prakhars117@gmail.com>
2020-04-08 17:28:04 +0530

    Create forum.js

---
+++ js/forum.js
@@ -0,0 +1,9 @@
+let afterHeader = document.getElementById('after-header');
+
+
+if (window.location.href === "https://forum.andronix.app") {
+    afterHeader.style.display = "block";
+}
+else {
+    afterHeader.style.display = "none";
+}
